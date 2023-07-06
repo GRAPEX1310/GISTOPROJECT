7c69071a3457c7e8c11172a016e62170328a4138
GRAPEX1310 <sashavin31@yandex.ru>
2023-07-06 17:39:54 +0300

    code: добавлен вывод гистограммы через svg

--- GISTOPROJECT.cpp
+++ GISTOPROJECT.cpp
@@ -155,11 +155,12 @@ void svg_text(double left, double baseline, string text)
 	cout << "<text x='" << left << "' y='" << baseline << "'>" << text <<"</text>";
 }
 
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
+void svg_rect(double x, double y, double width, double height, double fill_opacity, string stroke = "black", string fill = "black")
 {
 	cout << "<rect x='" << x << "' y='" << y
 		<< "' width='" << width << "' height='" << height 
-		<< "' stroke='" << stroke << "' fill='" << fill << "'/>";
+		<< "' stroke='" << stroke << "' fill='" << fill
+		<< "' fill-opacity='" << fill_opacity << "'/>";
 }
 
 void show_histogram_svg(const vector<size_t>& bins) 
@@ -171,22 +172,26 @@ void show_histogram_svg(const vector<size_t>& bins)
 	const auto TEXT_WIDTH = 50;
 	const auto BIN_HEIGHT = 30;
 	const auto BLOCK_WIDTH = 10;
+
 	svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+	
 	double top = 0;
+
+	size_t max_count = 0;
+
+	for (size_t i = 0; i < bins.size(); i++)
+	{
+		if (bins[i] > max_count) max_count = bins[i];
+	}
+	
 	for (size_t bin : bins) 
 	{
-		string bin_num = to_string(bin);
-
-		if (bin < 100)
-		{
-			bin_num = " " + bin_num;
-			if (bin < 10) bin_num = " " + bin_num;
-		}
-
 		const double bin_width = BLOCK_WIDTH * bin;
-		svg_text(TEXT_LEFT, top + TEXT_BASELINE, bin_num);
-		svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "black", "green");
+		const double fill_opacity = ((static_cast<double>(bin)) / max_count);
+		svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+		svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, fill_opacity, "black", "green");
 		top += BIN_HEIGHT;
+	
 	}
 	svg_end();
 }
