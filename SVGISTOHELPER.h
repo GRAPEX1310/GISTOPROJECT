cecf4dd04e7d2eb88349e216a99d8c3db7679c71
GRAPEX1310 <sashavin31@yandex.ru>
2023-07-09 14:08:42 +0300

    code: добавлены тесты для функции инд. варианта svg_rect

--- SVGISTOHELPER.h
+++ SVGISTOHELPER.h
@@ -9,6 +9,6 @@ void svg_end();
 
 void svg_text(double left, double baseline, string text);
 
-void svg_rect(double x, double y, double width, double height, double fill_opacity, string stroke = "black", string fill = "black");
+void svg_rect(double x, double y, double width, double height, double fill_opacity = 1, string stroke = "black", string fill = "green");
 
 void show_histogram_svg(const vector<size_t>& bins);
--- lab03-test/test.cpp
+++ lab03-test/test.cpp
@@ -1,6 +1,8 @@
 #include "histogram.h"
+#include "SVGISTOHELP.h"
 #include <cassert>
 
+//Tests for find_minmax()
 void test_positive() 
 {
 	double min = 0;
@@ -55,12 +57,70 @@ void test_multi_numbers()
 	assert(max == 234);
 }
 
+//Tests for svg_rect()
+
+void test_rect_full_opacity()
+{
+	const auto IMAGE_WIDTH = 400;
+	const auto IMAGE_HEIGHT = 300;
+	const auto TEXT_LEFT = 20;
+	const auto TEXT_BASELINE = 20;
+	const auto TEXT_WIDTH = 50;
+	const auto BIN_HEIGHT = 30;
+	const auto BLOCK_WIDTH = 10;
+	const auto SCREEN_WIDTH = 80;
+	const auto MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+	const double fill_opacity = 1;
+
+	svg_rect(TEXT_WIDTH, 0, BLOCK_WIDTH * 100, BIN_HEIGHT, fill_opacity);
+	assert(fill_opacity == 1);
+}
+
+void test_rect_zero_opacity()
+{
+	const auto IMAGE_WIDTH = 400;
+	const auto IMAGE_HEIGHT = 300;
+	const auto TEXT_LEFT = 20;
+	const auto TEXT_BASELINE = 20;
+	const auto TEXT_WIDTH = 50;
+	const auto BIN_HEIGHT = 30;
+	const auto BLOCK_WIDTH = 10;
+	const auto SCREEN_WIDTH = 80;
+	const auto MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+	const double fill_opacity = 0;
+	
+	svg_rect(TEXT_WIDTH, 0, BLOCK_WIDTH * 100, BIN_HEIGHT, fill_opacity);
+	assert(fill_opacity == 0);
+}
+
+void test_rect_default_opacity()
+{
+	const auto IMAGE_WIDTH = 400;
+	const auto IMAGE_HEIGHT = 300;
+	const auto TEXT_LEFT = 20;
+	const auto TEXT_BASELINE = 20;
+	const auto TEXT_WIDTH = 50;
+	const auto BIN_HEIGHT = 30;
+	const auto BLOCK_WIDTH = 10;
+	const auto SCREEN_WIDTH = 80;
+	const auto MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+	
+	svg_rect(TEXT_WIDTH, 0, BLOCK_WIDTH * 100, BIN_HEIGHT);
+	assert(fill_opacity == 0);
+}
 
 int main() 
 {
+	//Tests for find_minmax()
 	test_positive();
 	test_below_zero();
 	test_empty_array();
 	test_one_number();
 	test_same_numbers();
+	test_multi_numbers();
+
+	//Tests for svg_rect()
+	test_rect_full_opacity();
+	test_rect_zero_opacity();
+	test_rect_default_opacity();
 }
